7035a98494fb53b9e701e74cdda22eb4d62154d8
Gleb Petrakov <gleb.petrakov@gmail.com>
2020-12-06 12:57:40 +0300

    Init

---
+++ app/src/main/java/university/innopolis/madgroup3/onboarding/activities/LoginActivity.kt
@@ -0,0 +1,11 @@
+package university.innopolis.madgroup3.onboarding.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
