6332159f782071c3fe5c690afc8796ba570b90b5
Gleb Petrakov <gleb.petrakov@gmail.com>
2020-12-14 19:27:58 +0300

    Added logic behind CaptionActivity, fixed null on token request

--- app/src/main/java/university/innopolis/madgroup3/onboarding/activities/CaptionActivity.kt
+++ app/src/main/java/university/innopolis/madgroup3/onboarding/activities/CaptionActivity.kt
@@ -1,28 +1,12 @@
 package university.innopolis.madgroup3.onboarding.activities
 
-import android.content.SharedPreferences
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKey
-import io.reactivex.schedulers.Schedulers
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import university.innopolis.madgroup3.onboarding.OnboardingApplication
 import university.innopolis.madgroup3.onboarding.R
-import university.innopolis.madgroup3.onboarding.data.models.Checklist
-import university.innopolis.madgroup3.onboarding.data.models.Token
-import university.innopolis.madgroup3.onboarding.network.interceptors.AuthInterceptor
-import university.innopolis.madgroup3.onboarding.network.requests.TokenRequest
-import university.innopolis.madgroup3.onboarding.network.services.OnboardingAuthService
-import university.innopolis.madgroup3.onboarding.network.services.OnboardingDataService
 import university.innopolis.madgroup3.onboarding.data.repositories.ChecklistRepository
 import university.innopolis.madgroup3.onboarding.data.repositories.TokenRepository
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class CaptionActivity : AppCompatActivity() {
@@ -36,36 +20,26 @@ class CaptionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_caption)
 
-        // TODO: remove test queries
-        testAPI()
+        if (tokenRepository.getToken() == null) {
+            startActivityForResult(
+                Intent(this, LoginActivity::class.java),
+                LoginActivity.REQUEST_CODE
+            )
+        } else {
+            startMainActivity()
+            finish()
+        }
     }
 
-    private fun testAPI() {
-        val username = ""
-        val password = ""
+    private fun startMainActivity() = startActivity(Intent(this, MainActivity::class.java))
 
-        val token = tokenRepository.requestToken(username, password)
-        token ?: return showTokenFailToast()
-
-        val checklists = checklistRepository.getAllChecklists()
-        checklists ?: return showChecklistsFailToast()
-
-        Log.i("CaptionActivity", checklists.toString())
-    }
-
-    private fun showTokenFailToast() {
-        Toast.makeText(
-            this,
-            "Failed fetching a token",
-            Toast.LENGTH_SHORT,
-        ).show()
-    }
-
-    private fun showChecklistsFailToast() {
-        Toast.makeText(
-            this,
-            "Failed fetching checklists",
-            Toast.LENGTH_SHORT,
-        ).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            startMainActivity()
+            finish()
+        } else {
+            finish()
+        }
     }
 }
--- app/src/main/java/university/innopolis/madgroup3/onboarding/activities/LoginActivity.kt
+++ app/src/main/java/university/innopolis/madgroup3/onboarding/activities/LoginActivity.kt
@@ -1,32 +1,63 @@
 package university.innopolis.madgroup3.onboarding.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import university.innopolis.madgroup3.onboarding.OnboardingApplication
 import university.innopolis.madgroup3.onboarding.R
+import university.innopolis.madgroup3.onboarding.data.repositories.TokenRepository
+import javax.inject.Inject
 
 class LoginActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var tokenRepository: TokenRepository
+
+    private val mOnClickListener = View.OnClickListener {
+        val inputEmail = login_email.text.toString()
+        val inputPw = login_pw.text.toString()
+
+        val errorText = validateCredentials(inputEmail, inputPw)
+
+        if (!errorText.isNullOrBlank()) {
+            val errorToast = Toast.makeText(applicationContext, errorText, Toast.LENGTH_SHORT)
+            errorToast.show()
+            return@OnClickListener
+        }
+
+        login_btn.isEnabled = false
+        val token = tokenRepository.requestToken(inputEmail, inputPw)
+        login_btn.isEnabled = true
+
+        if (token == null) {
+            showToast("Failed logging in, try again or contact your admin")
+            login_pw.setText("")
+            return@OnClickListener
+        }
+
+        setResult(RESULT_OK)
+        finish()
+
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(
+            this,
+            message,
+            Toast.LENGTH_SHORT,
+        ).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as OnboardingApplication).appComponent.inject(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        login_btn.setOnClickListener {
-            val inputEmail = login_email.text.toString()
-            val inputPw = login_pw.text.toString()
-
-            val errorText = validateCredentials(inputEmail, inputPw)
-            if (!errorText.isNullOrBlank()) {
-                val errorToast = Toast.makeText(applicationContext, errorText, Toast.LENGTH_SHORT)
-                errorToast.show()
-            } else {
-                // TODO: API call with inputEmail and inputPw
-                // TODO: Fire an Intent after successful completion
-                // TODO: Fire a Toast on unsuccessful completion (error)
-            }
-        }
+        login_btn.setOnClickListener(mOnClickListener)
     }
 
     private fun validateCredentials(email: String, pw: String): String? {
@@ -49,5 +80,7 @@ class LoginActivity : AppCompatActivity() {
         const val ENTER_CREDENTIALS = "Please enter your credentials."
         const val VALID_EMAIL = "Please enter a valid e-mail address."
         const val SHORT_PW = "Please enter a password with more than 6 characters."
+
+        const val REQUEST_CODE = 1337
     }
 }
